2482e247b440654a91581281aa8258e610801406
comma-elsewhere <studioclear2@gmail.com>
2025-10-25 11:48:11 -0600

    made a shop + shopkeeper dialogue

---
+++ scripts/gui/shop_item.gd
@@ -0,0 +1,8 @@
+extends VBoxContainer
+
+@onready var name_label: Label = $HBoxContainer/Name
+@onready var price_label: Label = $HBoxContainer/Price
+
+
+func _on_button_button_up() -> void:
+	pass # Replace with function body.
